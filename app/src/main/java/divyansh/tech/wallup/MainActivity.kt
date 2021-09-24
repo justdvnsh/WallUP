687591b1333fd5ebbca069f794db39b2068366f2
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2021-09-24 16:55:14 +0530

    added fonts + color schemes and motion layout animations

--- app/src/main/java/divyansh/tech/wallup/MainActivity.kt
+++ app/src/main/java/divyansh/tech/wallup/MainActivity.kt
@@ -1,7 +1,12 @@
 package divyansh.tech.wallup
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -15,10 +20,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding.root)
         setupNavigation()
+        setupStatusBar()
     }
 
     private fun setupNavigation() {
@@ -29,5 +36,16 @@ class MainActivity : AppCompatActivity() {
             _binding.mainBottomNavigation,
             navController
         )
+    }
+
+    private fun setupStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER)
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+//            window.statusBarColor = ContextCompat.getColor(this, R.color.black)
+        }
     }
 }
